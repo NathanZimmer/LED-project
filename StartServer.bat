ff34899ec341c1402b123afccd92f90f5293ce47
Nathan Zimmer <96386265+NathanZimmer@users.noreply.github.com>
2022-02-09 20:27:06 -0500

    Rename start server.bat to StartServer.bat

---
+++ StartServer.bat
@@ -0,0 +1,1 @@
+plink -v -ssh pi@192.168.1.69 -pw 1234 sudo pkill pytho; sudo python3 RGB/RGB_server/rgb_server.py
--- start server.bat
+++
@@ -1,1 +0,0 @@
-plink -v -ssh pi@192.168.1.69 -pw 1234 sudo pkill pytho; sudo python3 RGB/RGB_server/rgb_server.py
